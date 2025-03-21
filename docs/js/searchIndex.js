71af2bd6e3e2f0d92aa29bcb5ffc74e12b53250a
frangj66 <Frangj66@gmail.com>
2025-03-21 09:21:52 -0700

    documentacio

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,24 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Mymath",
+            "name": "Mymath",
+            "summary": "",
+            "url": "classes/Mymath.html"
+        },                {
+            "fqsen": "\\Mymath\u003A\u003Asuma\u0028\u0029",
+            "name": "suma",
+            "summary": "",
+            "url": "classes/Mymath.html#method_suma"
+        },                {
+            "fqsen": "\\Mymath\u003A\u003Apot\u0028\u0029",
+            "name": "pot",
+            "summary": "",
+            "url": "classes/Mymath.html#method_pot"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
